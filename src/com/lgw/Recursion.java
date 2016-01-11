f72010013195432a091797471c582a39bf9f4b67
liguowei <guowei400@gmail.com>
2016-01-11 20:43:46 +0800

    递归求阶乘

---
+++ src/com/lgw/Recursion.java
@@ -0,0 +1,13 @@
+package com.lgw;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int getResult(int index) {
+		return 0;
+	}
+}

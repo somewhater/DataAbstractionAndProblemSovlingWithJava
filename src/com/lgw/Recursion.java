fbc6593471892f7f46fdf3333d14addc8dd8d438
liguowei <guowei400@gmail.com>
2016-01-13 18:14:11 +0800

    深入了解基本数据类型的范围解析

--- src/com/lgw/Recursion.java
+++ src/com/lgw/Recursion.java
@@ -1,6 +1,5 @@
 package com.lgw;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 
 /**
@@ -15,13 +14,13 @@ public class Recursion {
 		// TODO Auto-generated method stub
 		int reslut = getResultByInt(16);
 		System.out.println(reslut);
-		BigInteger bigInteger = getResultByInteger(100);
+		BigInteger bigInteger = getResultByInteger(1000);
 		System.out.println(bigInteger);
 	}
 
 	/**
 	 * 负数阶乘无意义，超过16则超过int范围，int类型占用4个字节 一个字节8bit，所以值的个数为2^32，高一位为正负表示，
-	 * +0和-0相等，所以int的范围是-2^31-1--------2^31-1
+	 * 原码表示范围-2^31----2^31,补码表示范围是-2^31--------2^31-1
 	 * 
 	 * @param index
 	 * @return
@@ -35,7 +34,11 @@ public class Recursion {
 			return index * getResultByInt(index - 1);
 		}
 	}
-
+	/**
+	 * 	BigInteger存值个数(2^(2147483647*4*8-1))-1
+	 * @param index
+	 * @return
+	 */
 	public static BigInteger getResultByInteger(int index) {
 		if (index < 0) {
 			return new BigInteger("0");
